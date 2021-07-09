4cdf03f9924757ca8f9afdfda2520ea76f8e0c8e
dhasamithra <74002230+dhasamithra@users.noreply.github.com>
2021-07-09 20:42:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+function preload(){
+    lips_undernose = loadedImage('https://i.postimg.cc/j5GNzF3D/Lips.png')
+}
+
+function steup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    image(video, 0, 0, 300, 300);
+}
+
+function take_snapshot()
+{
+    save('myprettyface.png');
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.lenght > 0)
+    {
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+}
